d063717b5f74b5cb7a8d4bb02e44e6f5b69072e2
Lokendra <lokilokendra12345@gmail.com>
2020-12-18 11:29:07 +0530

    [lokendra] first commit

---
+++ duplicateEntry/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace duplicateEntry
+{
+   
+         
+class program
+        {
+            static void Main()
+            {
+                int[] arr = { 1,2,3, 1, 1, 2, 2, 2, 3, 3,7,9,9, 4, 8, 5 };
+
+
+                arr = arr.Distinct().ToArray();
+
+                Array.ForEach(arr, x => Console.WriteLine(x));
+
+
+
+            }
+
+        }
+}
+    
+
